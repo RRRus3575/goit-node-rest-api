dff2bf67244f5e1ebb4463c5d4a7d056e9508b6b
Ruslan <ruslan.grudistov@gmail.com>
2025-04-03 16:41:19 +0300

    get, post, put, delete

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -3,11 +3,13 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
     name: Joi.string().required(), 
     email: Joi.string().email().required(), 
-    phone: Joi.string().pattern(/^\+?[0-9\s\-()]{10,20}$/).required()
+    phone: Joi.string().pattern(/^\+?[0-9\s\-()]{10,20}$/).required(),
+    favorite: Joi.boolean(),
 })
 
 export const updateContactSchema = Joi.object({
     name: Joi.string(), 
     email: Joi.string().email(), 
-    phone: Joi.string().pattern(/^\+?[0-9\s\-()]{10,20}$/)
+    phone: Joi.string().pattern(/^\+?[0-9\s\-()]{10,20}$/),
+    favorite: Joi.boolean(),
 })
