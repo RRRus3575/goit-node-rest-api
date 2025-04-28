304d6689f6985d706fa302f7459c263c77428574
Ruslan <ruslan.grudistov@gmail.com>
2025-04-28 11:25:11 +0300

    login verify

--- services/authServices.js
+++ services/authServices.js
@@ -52,6 +52,11 @@ export const loginUser = async(data) => {
         throw HttpError(401, "Email or password is wrong")
     }
 
+    if(!user.verify) {
+        throw HttpError(401, "Email not verified")
+    }
+
+
     const passwordCompare = await bcrypt.compare(password, user.password)
     if(!passwordCompare) {
         throw HttpError(401, "Email or password is wrong")
